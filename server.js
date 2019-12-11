cce7c79e307de7f26b66aee08d5da64e174dd2be
Miko <rootstax@gmail.com>
2019-12-11 13:21:39 -0600

    doing it live

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 //app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
